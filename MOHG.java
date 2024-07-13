0cd0427559a55cf31983e8d35176045c752e8286
caioeax <caio.ea@aluno.ifsc.edu.br>
2024-07-13 00:41:24 +0000

    done MOHG ROBOT

---
+++ MOHG.java
@@ -0,0 +1,27 @@
+package MOHG;
+
+import robocode.*;
+
+public class MOHG extends Robot {
+    public void run() {
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        back(10);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        back(20);
+    }
+}
