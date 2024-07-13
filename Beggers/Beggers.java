b7b7925fbf87d498c7789f06f7399294d775bcdb
caioeax <caio.ea@aluno.ifsc.edu.br>
2024-07-13 20:48:17 +0000

    Add Beggers code

---
+++ Beggers/Beggers.java
@@ -0,0 +1,28 @@
+package Beggers;
+
+import robocode.*;
+
+public class Beggers extends Robot {
+
+    public void run() {
+
+        while (true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        back(10);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        back(20);
+    }
+}
