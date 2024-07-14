8950cfe8c30e856465a9b8378d43c598684a108a
caioeax <caio.ea@aluno.ifsc.edu.br>
2024-07-14 23:04:30 +0000

    first improvement MOHG!

--- MOHG/MOHG.java
+++ MOHG/MOHG.java
@@ -1,30 +1,27 @@
 package MOHG;
 
 import robocode.*;
+import robocode.util.Utils;
 
 public class MOHG extends AdvancedRobot {
+    
     public void run() {
-        turnRadarRightRadians(Double.POSITIVE_INFINITY); 
-        do {
-            scan();
-        } while (true);
+        // gira o radar infinitamente ate encontrar um inimigo
+        setAdjustRadarForGunTurn(true); // *IMPORTANTE!!! deixa o radar independente da arma
+        while (true) {
+            turnRadarRight(Double.POSITIVE_INFINITY);
+        }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double radarTurn = getHeadingRadians() + e.getBearingRadians()
-                - getRadarHeadingRadians();
-        double fireTurn = getHeadingRadians() + e.getBearingRadians()
-                - getGunHeadingRadians();
-
-        setTurnGunRightRadians(Utils.normalRelativeAngle(fireTurn));
+        // crava a mira no inimigo
+        double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
         setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
+        
+        // Calcula a virada da arma pra targetar o inimigo
+        double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
+        double gunTurn = Utils.normalRelativeAngle(absoluteBearing - getGunHeadingRadians());
+        setTurnGunRightRadians(gunTurn);
+        
     }
-    // public void onFire(ScannedRobotEvent e) {
-    // double fireTurn =
-    // getHeadingRadians() + e.getBearingRadians()
-    // - getGunHeadingRadians();
-    // fire(1);
-
-    // setTurnGunRightRadians(Utils.normalRelativeAngle(fireTurn));
-    // }
 }
